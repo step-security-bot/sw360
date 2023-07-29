704705024abafe6ac5d84d2cccacdf90b7fcba0a
Helio Chissini de Castro <heliocastro@gmail.com>
2023-07-29 12:11:32 +0200

    feat(gradle): Initial bootstrap for new buildsystem
    Signed-off-by: Helio Chissini de Castro <heliocastro@gmail.com>

---
+++ buildSrc/src/main/kotlin/org.eclipse.sw360.java-conventions.gradle.kts
@@ -0,0 +1,39 @@
+/*
+ * Copyright Helio Chissini de Castro 2023
+ *
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+*/
+
+plugins {
+    `java-library`
+    `maven-publish`
+}
+
+repositories {
+    mavenLocal()
+    maven {
+        url = uri("https://repo.maven.apache.org/maven2/")
+    }
+}
+
+group = "org.eclipse.sw360"
+version = "17.99.0"
+java.sourceCompatibility = JavaVersion.VERSION_11
+
+publishing {
+    publications.create<MavenPublication>("maven") {
+        from(components["java"])
+    }
+}
+
+tasks.withType<JavaCompile>() {
+    options.encoding = "UTF-8"
+}
+
+tasks.withType<Javadoc>() {
+    options.encoding = "UTF-8"
+}
